097d895d9305f12cbcc963e2fb7cf382a34cd614
FiammaTri <scinty-pcd@live.it>
2024-12-02 18:27:54 +0100

    Aggiornati i commenti ed eliminati elementi inutilizzati

--- ParcoDivertimenti/src/main/ParcoDivertimenti.java
+++ ParcoDivertimenti/src/main/ParcoDivertimenti.java
@@ -1,41 +1,4 @@
-/*
- * Sistema di Simulazione per un Parco Divertimenti
-
-Progetta un programma in Java che simuli la gestione di un parco divertimenti. L’obiettivo è creare un sistema per monitorare l’affluenza, gestire le code alle attrazioni e calcolare ricavi e costi operativi.
-
-Requisiti
-Attrazioni:
-
-Ogni attrazione ha un nome, una capacità massima di persone, un costo operativo per giro, e un prezzo del biglietto per persona.
-Ogni attrazione può funzionare più volte al giorno, ma non può superare il numero massimo di giri giornalieri.
-
-Visitatori:
-
-Ogni visitatore ha un budget casuale assegnato all’ingresso e può scegliere quante attrazioni visitare in base ai prezzi.
-Se un’attrazione è piena, il visitatore può aspettare (se il tempo di attesa stimato è sotto un limite casuale assegnato) o scegliere un’altra attrazione.
-
-Turni di Gestione:
-
-Il parco è aperto per un numero definito di giorni.
-Ogni giorno si esegue una simulazione in cui:
-I visitatori entrano nel parco.
-Sceglieranno le attrazioni da visitare in base al budget e ai tempi di attesa.
-Gli incassi e i costi operativi delle attrazioni vengono calcolati.
-
-Statistiche Finali:
-
-Calcola:
-Il guadagno netto totale del parco.
-L’attrazione più visitata.
-Il tempo medio di attesa per attrazione.
-La percentuale di visitatori soddisfatti (che hanno potuto visitare almeno un'attrazione).
-
-Vincoli Tecnici
-
-Non usare array o strutture complesse come liste o mappe. Devi gestire tutto tramite variabili e cicli.
-Ogni attrazione e visitatore deve essere trattato singolarmente.
-Il codice deve essere ben commentato e includere spiegazioni per ogni sezione logica.
- */
+//GRUPPO 1: Fiamma Trillò, Giorgia Savioli, Nico Fiorini, Chiara Messina
 
 package main;
 
@@ -47,16 +10,14 @@ public class ParcoDivertimenti {
 
 		// Dati attrazioni
 
-		//giorni di apertura
+		//Giorni di apertura
 		String venerdì = "venerdì";
 		String sabato = "sabato";
 		String domenica = "domenica";
-		int giorno=0;
 
 		// GIOSTRA 1
 		String attrazione1 = "Tazze Rotanti";
 		int biglietto1 = 5;
-		int giriMax1 = 8;
 		int costoOp1 = 2;
 		int b1 = 0;
 
@@ -69,7 +30,6 @@ public class ParcoDivertimenti {
 		// GIOSTRA 3
 		String attrazione3 = "Bruco Mela";
 		int biglietto3 = 2;
-		int giriMax3 = 6;
 		int costoOp3 = 5;
 		int b3 = 0;
 
@@ -85,12 +45,11 @@ public class ParcoDivertimenti {
 		boolean vSoddisfatto=false;
 			
 		
-
 		Scanner scanner = new Scanner(System.in);
 
 
-		int sceltaGiornoRandom = (int) Math.random() * 5 + 1; // scelta random per verificare se il parco è
-																			// aperto
+		int sceltaGiornoRandom = (int) Math.random() * 5 + 1; 	// scelta random per verificare se il parco è aperto. Il numero è <7 per velocizzare i tentativi
+																//L'ideale era usare un array.
 		if (sceltaGiornoRandom==1) {
 		System.out.println(
 				"Benvenuti al parco divertimenti. Oggi è " + venerdì + " e il parco è aperto.");
@@ -108,17 +67,19 @@ public class ParcoDivertimenti {
 		for (numeroVisitatori = 0; numeroVisitatori < 10; numeroVisitatori++) {
 
 			System.out.println("Entra il visitatore n. " + (numeroVisitatori+1));
-			double budget = Math.random() * 4 + 2; // viene assegnato randomicamente il budget
+			double budget = Math.random() * 4 + 2; 		// viene assegnato randomicamente il budget
 			System.out.println("Il budget del visitatore è di: " + (int) budget + " euro.");
 
-			double attmax = Math.random() * 3 + 2; // viene assegnato randomicamente l'attesa massima del visitatore
+			double attmax = Math.random() * 3 + 2; 		// viene assegnato randomicamente l'attesa massima del visitatore
 			System.out.println("Il visitatore è disposto ad aspettare in fila al massimo " + (int) attmax + " minuti.");
 
-			double fila1 = Math.random() * 9 + 1; // viene assegnato randomicamente la fila
+			double fila1 = Math.random() * 9 + 1; 		// viene assegnato randomicamente la fila
 			double fila2 = Math.random() * 5 + 1;
 			double fila3 = Math.random() * 1 + 1;
 
 			System.out.println();
+			
+			//verifica per i visitatori soddisfatti. Se diventa vero 1 volta nel ciclo aggiunge 1 al numero totale
 			if (vSoddisfatto==true) {
 				soddisfattiTot+=1;
 			}
@@ -133,15 +94,16 @@ public class ParcoDivertimenti {
 				double scelta = Math.random() * 3 + 1; // in un intero per fare la scelta della giostra
 				scelta = Math.floor(scelta); // floor lo uso per trasformare il numero casuale
 
+				
 				// INIZIO CALCOLO ATTRAZIONI
 				// Attrazione 1
 				if (scelta == 1) {
 					
-					// controllo se l'attrazione è in funzione
-					attesaAtt1 += fila1; // aumento il calcolo della fila
-					if (budget >= biglietto1) { // e se non c'è troppa fila
+					
+					attesaAtt1 += fila1; // aumento il calcolo della fila per le statistiche
+					if (budget >= biglietto1) { // Controllo se può comprare il biglietto
 						
-						if (attmax >= fila1) { // se positivo procede, altrimenti cambia attrazione
+						if (attmax >= fila1) { // e se non c'è troppa fila
 							System.out.println("Decide di visitare l'attrazione " + attrazione1 + " e il costo è €"
 									+ biglietto1 + ".");
 							System.out.println("Acquista il biglietto e si mette in fila."); 
@@ -152,7 +114,7 @@ public class ParcoDivertimenti {
 							vSoddisfatto=true;
 							continue; // riparte il ciclo
 
-						} else if (attmax < fila1) {
+						} else if (attmax < fila1) {		//condizione se c'è troppa fila
 							System.out.println("Per " + attrazione1 + " c'è una fila di " + (int) fila1
 									+ " minuti. A causa della troppa fila decide di cambiare attrazione.");
 
@@ -168,11 +130,11 @@ public class ParcoDivertimenti {
 
 				else if (scelta == 2) {
 
-					attesaAtt2 += fila2;
-					// Controllo se può comprare il biglietto
-					if (budget >= biglietto2) { // e se non c'è troppa fila
+					attesaAtt2 += fila2;	// aumento il calcolo della fila per le statistiche
+					
+					if (budget >= biglietto2) { // Controllo se può comprare il biglietto
 						
-						if (attmax >= fila2) { // se positivo procede, altrimenti cambia attrazione
+						if (attmax >= fila2) { // e se non c'è troppa fila
 							System.out.println("Decide di visitare l'attrazione " + attrazione2 + " e il costo è €"
 									+ biglietto2 + ".");
 							System.out.println("Acquista il biglietto e si mette in fila."); 
@@ -184,7 +146,7 @@ public class ParcoDivertimenti {
 							
 							continue; // riparte il ciclo
 
-						} else if (attmax < fila2) {
+						} else if (attmax < fila2) {		//condizione se c'è troppa fila
 							System.out.println("Per " + attrazione2 + " c'è una fila di " + (int) fila2
 									+ " minuti. A causa della troppa fila decide di cambiare attrazione.");
 							System.out.println("");
@@ -198,10 +160,10 @@ public class ParcoDivertimenti {
 
 				else if (scelta == 3) {
 
-					attesaAtt3 += fila3;
-					// Controllo se può comprare il biglietto
-					if (budget >= biglietto3) { // e se non c'è troppa fila
-						if (attmax >= fila3) { // se positivo procede, altrimenti cambia attrazione
+					attesaAtt3 += fila3;	// aumento il calcolo della fila per le statistiche
+					
+					if (budget >= biglietto3) { // Controllo se può comprare il biglietto
+						if (attmax >= fila3) { // e se non c'è troppa fila
 							System.out.println("Decide di visitare l'attrazione " + attrazione3 + " e il costo è €"
 									+ biglietto3 + ".");
 							System.out.println("Acquista il biglietto e si mette in fila."); 
@@ -210,12 +172,12 @@ public class ParcoDivertimenti {
 							System.out.println("");
 							budget = budget - biglietto3;
 							b3++;
-							vSoddisfatto=true;
+							vSoddisfatto=true;		
 							
 							continue; // riparte il ciclo
 
 						}
-						if (attmax < fila3) {
+						if (attmax < fila3) {		//condizione se c'è troppa fila
 							System.out.println("Per " + attrazione3 + " c'è una fila di " + (int) fila3
 									+ " minuti. A causa della troppa fila decide di cambiare attrazione.");
 							System.out.println("");
@@ -223,23 +185,21 @@ public class ParcoDivertimenti {
 						}
 
 					}
-					tentativi3++; // arriva qui se il visitatore non ha abbastanza soldi o se c'è troppa fila
+					tentativi3++; 	// arriva qui se il visitatore non ha abbastanza soldi o se c'è troppa fila
 									// riparte da capo
 				} 
 
-			} while (budget >= biglietto1 && budget >= biglietto2 && budget >= biglietto3); // && stop1<=giriMax1 || budget>=b2 && stop2<=giriMax2
-																	// || budget>=b2 && stop3<=giriMax3);
+			} while (budget >= biglietto1 && budget >= biglietto2 && budget >= biglietto3);
 
 		}
 		
-		System.out.println("Visualizzare statistiche (1) o uscire(2)?");
+		System.out.println("Visualizzare statistiche (1) o uscire(2)?");			
 		statistica = scanner.nextInt();
 
 		if (statistica == 1) {
 			System.out.println(
-					"Il guadagno netto del giorno è: " + ((b1 - costoOp1) + (b2 - costoOp2) + (b3 - costoOp3))); // calcolo
-																													// guadagno
-																													// giornaliero
+					"Il guadagno netto del giorno è: " + ((b1 - costoOp1) + (b2 - costoOp2) + (b3 - costoOp3))); // calcolo guadagno giornaliero
+																													
 			if (b1 > b2 && b1 > b3) { // if per capire quale è stata l'attrazione più visitata tra la 1,2,3 prendendo
 										// come indicazione la parte precedente in cui il visitatore ha scelto le
 										// attrazioni
@@ -249,15 +209,15 @@ public class ParcoDivertimenti {
 			} else {
 				System.out.println("L'attrazione più visitata è: " + attrazione3 + ".");
 			}
-			System.out.println("Il tempo medio di attesa per " + attrazione1 + " è: " + (attesaAtt1 / (b1 + tentativi1))
+			System.out.println("Il tempo medio di attesa per " + attrazione1 + " è: " + (attesaAtt1 / (b1 + tentativi1))		//calcolo dei tempi medi di attesa per attrazione
 					+ " minuti.");
 			System.out.println("Il tempo medio di attesa per " + attrazione2 + " è: " + (attesaAtt2 / (b2 + tentativi2))
 					+ " minuti.");
 			System.out.println("Il tempo medio di attesa per " + attrazione3 + " è: " + (attesaAtt3 / (b3 + tentativi3))
 					+ " minuti.");
 			
-			double statSoddisfatti= (double)soddisfattiTot / (double) numeroVisitatori *100;
-			System.out.println("I visitatori sono soddisfatti per il " +((int)statSoddisfatti) + "%");
+			double statSoddisfatti= (double)soddisfattiTot / (double) numeroVisitatori *100;				//calcolo la media
+			System.out.println("I visitatori sono soddisfatti per il " +((int)statSoddisfatti) + "%");		// la trasformo in int
 			
 
 		} else
